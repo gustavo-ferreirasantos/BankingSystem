a417a33793f3a6c4a39784806f359818ac947784
GustavoFS <gustavoferreirasantosg@gmail.com>
2025-09-03 23:05:50 -0300

    Adiciona botão de logout nas telas Admin.fxml e Client.fxml

--- src/main/java/com/demo/bank/Controllers/AdminController.java
+++ src/main/java/com/demo/bank/Controllers/AdminController.java
@@ -1,4 +1,17 @@
 package com.demo.bank.Controllers;
 
+import com.demo.bank.App;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+
 public class AdminController {
+
+    @FXML
+    protected Button btExit;
+
+    @FXML
+    protected void Exit(){
+        App.setScene("Login");
+    }
 }
--- src/main/java/com/demo/bank/Controllers/ClientController.java
+++ src/main/java/com/demo/bank/Controllers/ClientController.java
@@ -1,5 +1,19 @@
 package com.demo.bank.Controllers;
 
+import com.demo.bank.App;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class ClientController {
+
+
+    @FXML
+    protected Button btExit;
+
+    @FXML
+    protected void Exit(){
+        App.setScene("Login");
+    }
+
 
 }
